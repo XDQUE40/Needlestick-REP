6f01c0c49bea9e07bbda46a5dee87a8add02b030
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-04 13:42:13 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -84,7 +84,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -92,7 +92,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
+		
 
 		#if html5
 		FlxG.autoPause = false;
